82c9e9b05ed3f5e9724745e719a4e6a3421e5d63
xiaolin <462441355@qq.com>
2018-02-05 14:00:19 +0800

    * 双向绑定测试

--- MVVM/js/observer.js
+++ MVVM/js/observer.js
@@ -1,38 +1,82 @@
-var data = {name: 'kindeng'}
-observe(data)
-data.name = 'dmq' // 哈哈哈，监听到值变化了 kindeng --> dmq
+function Observer (data) {
+  this.data = data
+  this.walk(data)
+}
 
-/**
- * 遍历对象
- * @param {*} data
- */
-function observe (data) {
-  if (!data || typeof data !== 'object') {
+Observer.prototype = {
+  walk: function (data) {
+    var me = this
+    Object.keys(data).forEach(function (key) {
+      me.convert(key, data[key])
+    })
+  },
+  convert: function (key, val) {
+    this.defineReactive(this.data, key, val)
+  },
+
+  defineReactive: function (data, key, val) {
+    var dep = new Dep()
+    var childObj = observe(val)
+
+    Object.defineProperty(data, key, {
+      enumerable: true, // 可枚举
+      configurable: false, // 不能再define
+      get: function () {
+        if (Dep.target) {
+          dep.depend()
+        }
+        return val
+      },
+      set: function (newVal) {
+        if (newVal === val) {
+          return
+        }
+        val = newVal
+        // 新的值是object的话，进行监听
+        childObj = observe(newVal)
+        // 通知订阅者
+        dep.notify()
+      }
+    })
+  }
+}
+
+function observe (value, vm) {
+  if (!value || typeof value !== 'object') {
     return
   }
-  // 取出所有属性遍历
-  Object.keys(data).forEach(function (key) {
-    defineReactive(data, key, data[key])
-  })
+
+  return new Observer(value)
+};
+
+var uid = 0
+
+function Dep () {
+  this.id = uid++
+  this.subs = []
 }
 
-/**
- * 监听对象
- * @param {*} data
- * @param {*} key
- * @param {*} val
- */
-function defineReactive (data, key, val) {
-  observe(val) // 监听子属性
-  Object.defineProperty(data, key, {
-    enumerable: true, // 可枚举
-    configurable: false, // 不能再define
-    get: function () {
-      return val
-    },
-    set: function (newVal) {
-      console.log('哈哈哈，监听到值变化了 ', val, ' --> ', newVal)
-      val = newVal
+Dep.prototype = {
+  addSub: function (sub) {
+    this.subs.push(sub)
+  },
+
+  depend: function () {
+    Dep.target.addDep(this)
+  },
+
+  removeSub: function (sub) {
+    var index = this.subs.indexOf(sub)
+    if (index != -1) {
+      this.subs.splice(index, 1)
     }
-  })
+  },
+
+  notify: function () {
+    this.subs.forEach(function (sub) {
+      sub.update()
+    })
+  }
 }
+
+Dep.target = null
