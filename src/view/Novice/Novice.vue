2648d0596ab2ab899601a54e992f34e7bb81487e
xiaolin <462441355@qq.com>
2018-02-07 23:27:29 +0800

    * 引入内容

--- src/view/Novice/Novice.vue
+++ src/view/Novice/Novice.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
       <Panel title="标题" :class="$style.panel">
-          内容测试
+          <section :class="$style.content">
+            <div :class="$style.item">
+                <h4>下载APP<span :class="$style.red">送888元礼包</span></h4>
+                <p :class="$style.gray">新手专享</p>
+                <img src="//img12.360buyimg.com/jrpmobile/jfs/t10384/155/1759179594/9776/185bd062/59e5f0ebNec4cf494.png?width=100&height=100" alt="">
+            </div>
+            <div :class="$style.item">
+                <ul>
+                    <li>
+                        <img src="//img12.360buyimg.com/jrpmobile/jfs/t4639/162/1782623297/60754/98ea03b4/58e60952N7fdb2b85.png?width=200&height=200" alt="">
+                        <h4 :class="$style.red">领128元新手礼包</h4>
+                        <p :class="$style.gray">立即开通</p>
+                    </li>
+                    <li>
+                        <img src="//img12.360buyimg.com/jrpmobile/jfs/t4804/179/657713323/5908/66dce262/58e6095fNd3dc3f39.png?width=100&height=100" alt="">
+                        <h4>嘿嘿<span :class="$style.red">享免息</span></h4>
+                        <p :class="$style.gray">立即开通享豪礼</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
       </Panel>
   </div>
 </template>
